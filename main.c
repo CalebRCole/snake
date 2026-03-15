f98fdcdb060ade8bda1e5a69240e66b23d002388
Caleb Cole <calebrylancole@gmail.com>
2026-03-15 02:23:11 -0400

    feat: Create and set initial Snake values/memory
    Draws walls and then initializes the snake. Should spawn in the middle
    of the map.

--- main.c
+++ main.c
@@ -8,19 +8,25 @@
 int main() {
   draw_walls();
 
+  // Two segments are created to start. Has benefit of avoiding
+  // a segmentation fault in checking for snake->head->next.
   struct Snake *snake = malloc(sizeof(struct Snake));
   struct Segment *initial_segment = malloc(sizeof(struct Segment));
+  struct Segment *second_segment = malloc(sizeof(struct Segment));
 
   snake->head = initial_segment;
   snake->length = 1;
 
   initial_segment->x = WIDTH / 2;
   initial_segment->y = HEIGHT / 2;
+  initial_segment->next = second_segment;
 
   while (!close) {
+    bool ate = false;
+
     input();
     logic();
-    drawing();
+    draw_movement(snake, ate);
   }
 
   return EXIT_SUCCESS;
